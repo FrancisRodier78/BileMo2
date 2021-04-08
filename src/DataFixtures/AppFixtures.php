ff7527fd44edaf52ea115850c3a99fb71c0854b4
Rodier <francisrodier78@yahoo.fr>
2021-04-08 17:59:35 +0200

    Mise en place de la base de données.

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Client;
+use App\Entity\Produit;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * L'encoder de mot de passe
+     *
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        for($i = 1; $i <= 12; $i++) {
+            $produit = new Produit();
+
+            $nom = $faker->sentence(1);
+            $description = $faker->sentence(9);
+
+            $produit->setNom($nom)
+                ->setDescription($description);
+
+            $manager->persist($produit);
+        }
+
+        $client = new Client();
+
+        $hash = $this->encoder->encodePassword($client, "password");
+
+        $client->setNom('SFR');
+        $client->setDescription('Opérateur SFR.');
+        $client->setEmail('SFR@yahoo.fr');
+        $client->setPassword($hash);
+        $client->setRoles([]);
+
+        $manager->persist($client);
+
+        for($i = 1; $i <= 5; $i++) {
+            $user = new User();
+
+            $prenom = $faker->firstNameMale;
+            $nom = $faker->lastName;
+            $adresse = $faker->address;
+            $description = $faker->sentence(7);
+
+            $user->setPrenom($prenom)
+                ->setNom($nom)
+                ->setAdresse($adresse)
+                ->setDescription($description)
+                ->setClient($client);
+
+            $manager->persist($user);
+        }
+
+        $client = new Client();
+
+        $hash = $this->encoder->encodePassword($client, "password");
+
+        $client->setNom('Orange');
+        $client->setDescription('Opérateur Orange.');
+        $client->setEmail('Orange@yahoo.fr');
+        $client->setPassword($hash);
+        $client->setRoles([]);
+
+        $manager->persist($client);
+
+        for($i = 1; $i <= 7; $i++) {
+            $user = new User();
+
+            $prenom = $faker->firstNameMale;
+            $nom = $faker->lastName;
+            $adresse = $faker->address;
+            $description = $faker->sentence(7);
+
+            $user->setPrenom($prenom)
+                ->setNom($nom)
+                ->setAdresse($adresse)
+                ->setDescription($description)
+                ->setClient($client);
+
+            $manager->persist($user);
+        }
+
+        $client = new Client();
+
+        $hash = $this->encoder->encodePassword($client, "password");
+
+        $client->setNom('Bouygues');
+        $client->setDescription('Opérateur Bouygues.');
+        $client->setEmail('Bouygues@yahoo.fr');
+        $client->setPassword($hash);
+        $client->setRoles([]);
+
+        $manager->persist($client);
+
+        for($i = 1; $i <= 9; $i++) {
+            $user = new User();
+
+            $prenom = $faker->firstNameMale;
+            $nom = $faker->lastName;
+            $adresse = $faker->address;
+            $description = $faker->sentence(7);
+
+            $user->setPrenom($prenom)
+                ->setNom($nom)
+                ->setAdresse($adresse)
+                ->setDescription($description)
+                ->setClient($client);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
+
---
+++ src/Entity/User.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Client::class, inversedBy="users")
+     */
+    private $client;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getClient(): ?Client
+    {
+        return $this->client;
+    }
+
+    public function setClient(?Client $client): self
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+}
