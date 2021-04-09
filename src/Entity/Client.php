543272902b4e1dde44496f1d841eaeb224556725
Rodier <francisrodier78@yahoo.fr>
2021-04-09 17:55:16 +0200

    Mise en place de la pagination.

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -2,14 +2,16 @@
 
 namespace App\Entity;
 
-use App\Repository\ClientRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ClientRepository;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=ClientRepository::class)
+ * @ApiResource
  */
 class Client implements UserInterface
 {
--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\ProduitRepository;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ProduitRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=ProduitRepository::class)
+ * @ApiResource
  */
 class Produit
 {
